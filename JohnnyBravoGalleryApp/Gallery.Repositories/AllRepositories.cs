611953dcdb74fdd5613b7087ab372038d30f6b6f
Bobby Kolev <bobbykolev@abv.bg>
2013-08-14 03:15:05 -0700

    repos

---
+++ JohnnyBravoGalleryApp/Gallery.Repositories/AllRepositories.cs
@@ -0,0 +1,36 @@
+﻿using Gallery.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery.Repositories
+{
+    public class AllRepositories
+    {
+        public AllRepositories(
+            IRepository<Album> albumRepo,
+            IRepository<Image> imageRepo,
+            IRepository<User> userRepo, 
+            IRepository<Gallery.Data.Gallery> galleryRepo,
+            IRepository<Comment> commentRepo)
+        {
+            this.ImageRepo = imageRepo;
+            this.AlbumRepo = albumRepo;
+            this.GalleryRepo = galleryRepo;
+            this.UserRepo = userRepo;
+            this.CommentRepo = commentRepo;
+        }
+
+        public IRepository<Image> ImageRepo { get; private set; }
+
+        public IRepository<Album> AlbumRepo { get; private set; }
+
+        public IRepository<Gallery.Data.Gallery> GalleryRepo { get; private set; }
+
+        public IRepository<User> UserRepo { get; private set; }
+
+        public IRepository<Comment> CommentRepo { get; private set; }
+    }
+}
---
+++ JohnnyBravoGalleryApp/Gallery.Repositories/EfRepository.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery.Repositories
+{
+    public class EfRepository<T> : IRepository<T> where T : class
+    {
+        protected IDbSet<T> DbSet { get; set; }
+        protected DbContext Context { get; set; }
+
+        public EfRepository(DbContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentException("An instance of DbContext is required to use this repository.", "context");
+            }
+
+            this.Context = context;
+            this.DbSet = this.Context.Set<T>();
+        }
+
+        public void Add(T item)
+        {
+            DbEntityEntry entry = this.Context.Entry(item);
+            if (entry.State != EntityState.Detached)
+            {
+                entry.State = EntityState.Added;
+            }
+            else
+            {
+                this.DbSet.Add(item);
+            }
+
+            this.Context.SaveChanges();
+        }
+
+        public virtual void Delete(T entity)
+        {
+            DbEntityEntry entry = this.Context.Entry(entity);
+            if (entry.State != EntityState.Deleted)
+            {
+                entry.State = EntityState.Deleted;
+            }
+            else
+            {
+                this.DbSet.Attach(entity);
+                this.DbSet.Remove(entity);
+            }
+
+            this.Context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var entity = this.Get(id);
+
+            if (entity != null)
+            {
+                this.Delete(entity);
+            }
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return this.DbSet;
+        }
+
+        public T Get(int id)
+        {
+            return this.DbSet.Find(id);
+        }
+
+        public void Update(int id, T item)
+        {
+            DbEntityEntry entry = this.Context.Entry(item);
+            if (entry.State == EntityState.Detached)
+            {
+                this.DbSet.Attach(item);
+            }
+
+            entry.State = EntityState.Modified;
+
+            this.Context.SaveChanges();
+        }
+    }
+}
