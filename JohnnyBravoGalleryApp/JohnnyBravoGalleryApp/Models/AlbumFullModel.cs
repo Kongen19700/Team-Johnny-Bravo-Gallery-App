5979107a0b980ae310bed2724fcaccd048e62ed2
bahtev <viktorbahtev@gmail.com>
2013-08-14 15:26:53 +0300

    Fix

--- JohnnyBravoGalleryApp/JohnnyBravoGalleryApp/Models/AlbumFullModel.cs
+++ JohnnyBravoGalleryApp/JohnnyBravoGalleryApp/Models/AlbumFullModel.cs
@@ -7,9 +7,9 @@ namespace JohnnyBravoGalleryApp.Models
 {
     public class AlbumFullModel : AlbumModel
     {
-        public ICollection<AlbumModel> SubAlbums { get; set; }
+        public IEnumerable<AlbumModel> SubAlbums { get; set; }
         public AlbumModel ParentAlbum { get; set; }
         public GalleryModel Gallery { get; set; }
-        public ICollection<ImageModel> Images { get; set; }
+        public IEnumerable<ImageModel> Images { get; set; }
     }
 }
